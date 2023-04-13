972cbfa69799180d4607ae84bbd8c03febbac117
lattecoff <atkbvs@gmail.com>
2023-04-13 23:11:56 +0300

    Update mainpage.py .

--- mainpage.py
+++ mainpage.py
@@ -4,10 +4,17 @@ mainpage = Flask(__name__)
 
 
 @mainpage.route("/")
+@mainpage.route("/mainpage")
 def index():
-	return "hello world"
+	return "Hello world"
 
+@mainpage.route("/about")
+def about():
+	return "About"
 
+@mainpage.route("/user/<string:name>/<int:id>")
+def user(name, id):
+	return "User: " + name + "-" + id
 
 if __name__ == "__main__":
 	mainpage.run(debug=True)
