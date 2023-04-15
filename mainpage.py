6166a284803bf6ed35c8e056ffa3708f00bc63b8
lattecoff <atkbvs@gmail.com>
2023-04-15 20:27:43 +0300

    Added pages: base, mainpage, about. Include Bootstrap styles

--- mainpage.py
+++ mainpage.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 mainpage = Flask(__name__)
 
@@ -6,15 +6,15 @@ mainpage = Flask(__name__)
 @mainpage.route("/")
 @mainpage.route("/mainpage")
 def index():
-	return "Hello world"
+	return render_template("mainpage.html")
 
 @mainpage.route("/about")
 def about():
-	return "About"
+	return render_template("about.html")
 
 @mainpage.route("/user/<string:name>/<int:id>")
 def user(name, id):
-	return "User: " + name + "-" + id
+	return "User: " + name + "-" + str(id)
 
 if __name__ == "__main__":
 	mainpage.run(debug=True)
