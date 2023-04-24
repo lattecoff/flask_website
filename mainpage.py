b2384dfbef4750240f8cf69d2f2d892e83a787ce
lattecoff <atkbvs@gmail.com>
2023-04-24 15:07:05 +0300

    Added data base. Added model of articles.

--- mainpage.py
+++ mainpage.py
@@ -1,6 +1,23 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
 
 mainpage = Flask(__name__)
+mainpage.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///blog.db"
+mainpage.config["SQLALCHEMY_TRACK_MODIFICATION"] = False
+db = SQLAlchemy(mainpage)
+
+
+class Article(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	title = db.Column(db.String(100), nullable=False)
+	intro = db.Column(db.String(300), nullable=False)
+	text = db.Column(db.Text, nullable=False)
+	date = db.Column(db.DateTime(300), default=datetime.utcnow)
+
+	def __repr__(self):
+		return "<Article %r>" %self.id
 
 
 @mainpage.route("/")
@@ -11,6 +28,27 @@ def index():
 @mainpage.route("/about")
 def about():
 	return render_template("about.html")
+
+@mainpage.route("/create-article.html", methods=["POST", "GET"])
+#@mainpage.route("/create-article.html")
+def create_article():
+	if request.method == "POST":
+		title = request.form["title"]
+		intro = request.form["intro"]
+		text = request.form["text"]
+
+		article = Article(title=title, intro=intro, text=text)
+
+		try:
+			db.session.add(article)
+			db.session.commit()
+			return redirect("/")
+
+		except:
+			return "Error."
+	else:
+		return render_template("create-article.html")
+
 
 @mainpage.route("/user/<string:name>/<int:id>")
 def user(name, id):
